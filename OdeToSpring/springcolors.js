5581128b0350e505e22839b81b872d69ffe2f535
lizzychiappini <echiappini1@gmail.com>
2021-05-10 12:21:49 -0400

    odetospring

---
+++ OdeToSpring/springcolors.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+// alert("Im the JS file");
+      $("#IMG_1").draggable();
+      $("#IMG_2").draggable();
+      $("#IMG_3").draggable();
+      $("#IMG_4").draggable();
+      $("#IMG_5").draggable();
+      $("#IMG_6").draggable();
+      $("#IMG_7").draggable();
+      $("#IMG_8").draggable();
+      $("#IMG_9").draggable();
+      $("#IMG_10").draggable();
+      $("#IMG_11").draggable();
+      $("#IMG_12").draggable();
+      $("#IMG_13").draggable();
+      $("#IMG_14").draggable();
+      $("#IMG_15").draggable();
+      $("#IMG_16").draggable();
+      $("#IMG_17").draggable();
+      $("#IMG_18").draggable();
+      $("#IMG_19").draggable();
+      $("#IMG_20").draggable();
+      $("#IMG_21").draggable();
+      $("#IMG_22").draggable();
+      $("#IMG_23").draggable();
+      $("#IMG_24").draggable();
+      $("#IMG_25").draggable();
+      $("#IMG_26").draggable();
+      $("#IMG_27").draggable();
+      $("#IMG_28").draggable();
+      $("#IMG_29").draggable();
+      $("#IMG_30").draggable();
+      $("#springFeels").draggable();
+
+})
